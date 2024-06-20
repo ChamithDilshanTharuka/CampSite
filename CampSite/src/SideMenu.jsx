af2e909a7adde26f7ad21190d6b7080ad279ebe3
Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-06-20 18:37:19 +0530

    side menu updated

--- CampSite/src/SideMenu.jsx
+++ CampSite/src/SideMenu.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const SideMenu = () => {
   return (
-   <div className='container-fluid'>
+
     <div className='aside'>
-    <div className="p-3 text-white bg-dark col-auto">
-      <span className="fs-4">Product Categories</span>
+    <div className="p-4 text-white">
+      <span className="fs-2">Product Categories</span>
    
     <hr/>
-    <ul className="nav nav-pills flex-column mb-auto ">
+    <ul className="nav nav-pills flex-column">
       <li className="nav-item">
         <a href="#" className="nav-link active" aria-current="page">
           Home
@@ -39,21 +39,6 @@ const SideMenu = () => {
         </a>
       </li>
     </ul>
-    <hr/>
-    <div className="dropdown">
-      <a href="#" className="d-flex align-items-center text-white
-       text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <strong>Profile</strong>
-      </a>
-      <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-        <li><a className="dropdown-item" href="#">New project...</a></li>
-        <li><a className="dropdown-item" href="#">Settings</a></li>
-        <li><a className="dropdown-item" href="#">Profile</a></li>
-        <li><hr className="dropdown-divider"/></li>
-        <li><a className="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
-  </div>
   </div>
   </div>
   )
