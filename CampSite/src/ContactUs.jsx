9ff20d3e89d0fff6c8fb80fff80d4e8eee707741
Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-06-24 12:59:34 +0530

    "Updated App.css and ContactUs.jsx: removed background color, added overlay text and subtext styles, and modified contact data layout with image overlay"

--- CampSite/src/ContactUs.jsx
+++ CampSite/src/ContactUs.jsx
@@ -15,6 +15,7 @@ function ContactUs() {
           <Form>
           <div className='form-title'>Write to Us</div>
           <div className='form-subtitle'>We will get back to you as soon as possible...</div>
+          <hr />
             <Form.Group className="mb-3">
               <Form.Label For="TextInput">Full Name</Form.Label>
               <Form.Control id="TextInput" placeholder="Full Name" />
@@ -29,15 +30,24 @@ function ContactUs() {
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label For="TextInput">Message</Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder="Enter your message here" />
+              <Form.Control as="textarea" rows={5} placeholder="Enter your message here" />
             </Form.Group>
             <Button type="submit">Submit</Button>
             <div className='form-last'>**We are committed to protecting your privacy.
             </div>
         </Form></div>
-          <div className='contact-data'>
-      <img src="SmallBanner/small-banner1.jpg" className="card-img-top" alt="backpackImg"/>
-              <h1>Contact Us</h1>
+    <div className='contact-data'>
+      <div className='image-container'>
+          <img src="SmallBanner/contact.jpg" className="card-img-top" alt="backpackImg"/>
+          <div className="overlay-text">Contact Us</div>
+          
+          <div className='overlay-subtext-container'>
+            <a href='https://maps.app.goo.gl/6utt6wVdjSTGwLHv5' target='_blank' className='overlay-subtext'>📍 Address: Kothmale, Sri Lanka</a>
+            <a href="tel:+0528987932" className='overlay-subtext'>📞 Phone: 052- 8987932</a>
+            <a href="mailto:campsitecontact@gmail.com" className='overlay-subtext'>📧 Email: campsitecontact@gmail.com</a>
+          </div>
+          
+      </div>
         </div>
      </div>
    </div>
