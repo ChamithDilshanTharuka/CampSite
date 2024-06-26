5d378a73e841fa25c841cc4bf3f1a406d44bcb4a
Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-06-27 00:12:57 +0530

    about us page changes

--- CampSite/src/AboutUs.jsx
+++ CampSite/src/AboutUs.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 import Header from './Header.jsx'
 import Button from 'react-bootstrap/Button';
 import Footer from './Footer.jsx'
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Card from 'react-bootstrap/Card';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import TabsBar from './TabsBar.jsx'
 
 function AboutUs() {
   return (
@@ -21,7 +15,7 @@ function AboutUs() {
      <div className='about-image-container'>
         <img src="/SmallBanner/aboutImg.jpg" alt="aboutImg" />
      </div>
-     <div className='overlay-subtext-container-about'>
+     <div className='overlay-subtext-container-about my-3'>
           <h1>We are the No.1 camping Providers in Sri lanka.</h1>
           <p>At CAMPSITE, we pride ourselves on being the premier provider of high-quality
             camping gear and accessories in Sri Lanka.
@@ -31,53 +25,9 @@ function AboutUs() {
             <div className='learn-more'>
              <Button variant="outline-info" href="https://www.jungletide.com/a-trip-to-kotmale/" type="button" size="lg">Learn More</Button>
             </div>
-          </div>
-
-          <div className='tabs'>
-          <Tabs defaultActiveKey="commitment" id="justify-tab-example" className="mb-3" justify>
-      <Tab eventKey="commitment" title="Our Commitment">
-        <Container>
-          <Row>
-            <Col md={4}>
-              <Card.Img variant="top" src="path/to/your/image1.jpg" />
-            </Col>
-            <Col md={8}>
-              <Card>
-                <Card.Body>
-                  <Card.Title>Our Commitment</Card.Title>
-                  <Card.Text>
-                    We are dedicated to providing the highest quality camping gear and accessories. Our mission is to ensure that every outdoor enthusiast has access to reliable, durable, and affordable equipment to enhance their adventure experiences.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </Tab>
-                <Tab eventKey="ContactUs.jsx" title="Profile">
-                  Tab content for Profile
-                </Tab>
-                <Tab eventKey="longer-tab" title="Loooonger Tab">
-                  Tab content for Loooonger Tab
-                </Tab>
-                <Tab eventKey="contact" title="Contact">
-                  Tab content for Contact
-                </Tab>
-          </Tabs>
-          </div>
-
-          <div className='commitment'>
-            <h1>Our Commitment</h1>
-          </div>
-          <div className='whyus'>
-            <h1>Why Choose Us?</h1>
-          </div>
-          <div className='our-story'>
-            <h1>Our Story</h1>
-          </div>
-          <div className='joinus'>
-            <h1>Join Or Community</h1>
-          </div>
+      </div>
+      <TabsBar/>
+ 
       </div>
 
    <div className='footer-container'>
---
+++ CampSite/src/TabsBar.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function TabsBar() {
+    return (
+<div className='tabs'>
+  
+<Tabs  className="mt-3" justify>
+      <Tab className='A-tab' eventKey="commitment" title="Our Commitment">
+      <Container className='my-5 conatainer-card'>
+      <Row className='row-card p-3'>
+        <Col lg={4}>
+          <Card.Img variant="top" src="AboutUsImg/about-1.jpg" />
+        </Col>
+        <Col lg={8}>
+          <Card className='card-data'>
+            <Card.Body>
+              <Card.Title>Our Commitment</Card.Title>
+              <Card.Text>
+               <div>
+               <b>Quality and Durability</b>: We understand that when you’re out in the wild, you need
+                equipment that you can rely on. That’s why we only stock products from trusted brands 
+                that meet our rigorous standards for quality and durability.
+                </div> 
+                <br />
+                <div>
+                <b>Wide Range of Products</b>: Whether you're planning a weekend getaway or an extended 
+                expedition, we have everything you need to make your camping experience unforgettable.
+                 From tents, sleeping bags, and backpacks to cooking gear, lighting, and safety equipment,
+                  our extensive range ensures you're well-prepared for any adventure.
+                </div> 
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </Container>
+      </Tab>
+
+      <Tab className='A-tab' eventKey="whyus" title="Why Choose Us?">
+      <Container className='my-5 conatainer-card'>
+      <Row className='row-card p-3'>
+        <Col lg={4}>
+          <Card.Img variant="top" src="AboutUsImg/about-1.jpg" />
+        </Col>
+        <Col lg={8}>
+          <Card className='card-data'>
+            <Card.Body>
+              <Card.Title>Why Choose Us?</Card.Title>
+              <Card.Text>
+                <div>
+                <b>Customer-Centric Approach:</b> Your satisfaction is our top priority.
+                 We strive to provide a seamless shopping experience from start to finish,
+                  with easy navigation, detailed product descriptions, and a hassle-free return policy.
+                </div>
+                <br />
+                <div>
+                <b>Competitive Prices:</b> We believe that top-quality camping gear should be accessible to everyone. 
+                That’s why we offer competitive prices and frequent promotions to ensure you get the best value for your money.
+                </div>
+                 </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </Container>
+      </Tab> 
+
+      <Tab className='A-tab' eventKey="ourstory" title="Our Story">
+      <Container className='my-5 conatainer-card'>
+      <Row className='row-card p-3'>
+        <Col lg={4}>
+          <Card.Img variant="top" src="AboutUsImg/about-1.jpg" />
+        </Col>
+        <Col lg={8}>
+          <Card className='card-data'>
+            <Card.Body>
+              <Card.Title>Our Commitment</Card.Title>
+              <Card.Text>
+                We are dedicated to providing the highest quality camping gear and accessories. Our mission is to ensure that every outdoor enthusiast has access to reliable, durable, and affordable equipment to enhance their adventure experiences.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </Container>
+      </Tab>
+
+      <Tab className='A-tab' eventKey="commitment" title="Our Commitment">
+      <Container className='my-5 conatainer-card'>
+      <Row className='row-card p-3'>
+        <Col lg={4}>
+          <Card.Img variant="top" src="AboutUsImg/about-1.jpg" />
+        </Col>
+        <Col lg={8}>
+          <Card className='card-data'>
+              <Card.Body>
+                <Card.Title>Our Commitment</Card.Title>
+                <Card.Text>
+                  We are dedicated to providing the highest quality camping gear and accessories. Our mission is to ensure that every outdoor enthusiast has access to reliable, durable, and affordable equipment to enhance their adventure experiences.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        </Container>
+        </Tab>
+              
+</Tabs>
+</div>
+        );
+    }
+
+export default TabsBar;
